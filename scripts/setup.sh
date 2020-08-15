c112442547d2995e967db75c2ced083a6823865e
SnO2WMaN <sno2wman.official@gmail.com>
2020-08-15 20:27:13 +0900

    vimplug追加

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,28 +13,32 @@ done
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew bundle --global
 
+# vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # asdf
 ## python
 asdf plugin-add python
-asdf install $(cat ~/.tool-versions | grep python)
+asdf install $(cat $DOTFILES/.tool-versions | grep python)
 
 ## golang
 asdf plugin-add golang
-asdf install $(cat ~/.tool-versions | grep golang)
+asdf install $(cat $DOTFILES/.tool-versions | grep golang)
 
 ## ruby
 asdf plugin-add ruby
-asdf install $(cat ~/.tool-versions | grep ruby)
+asdf install $(cat $DOTFILES/.tool-versions | grep ruby)
 
 ## nodejs
 asdf plugin-add nodejs
 bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
-asdf install $(cat ~/.tool-versions | grep nodejs)
+asdf install $(cat $DOTFILES/.tool-versions | grep nodejs)
 
 ## ocaml
 asdf plugin-add ocaml
-asdf install $(cat ~/.tool-versions | grep ocaml)
+asdf install $(cat $DOTFILES/.tool-versions | grep ocaml)
 
 ## haxe
 asdf plugin-add haxe
-asdf install $(cat ~/.tool-versions | grep haxe)
+asdf install $(cat $DOTFILES/.tool-versions | grep haxe)
